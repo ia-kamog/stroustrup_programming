f69c8f5bdbce084aa4b46e6ca6fb732ca0e8f395
Art Kamog <ia.kamog@ya.ru>
2018-06-16 01:50:46 +0300

    Exercise 7.11: cleanup an exercise from earlier chapters

--- ch5/ex6.cpp
+++ ch5/ex6.cpp
@@ -4,21 +4,26 @@
 
 #include "std_lib_facilities.h"
 
+// degrees of fahrenheit in a degree of celsius
 constexpr double F_in_C = 9.0 / 5.0;
+// 0 C on the Fahrenheit scale
 constexpr double zeroC_in_F = 32.0;
 
 static double ftoc(double);
 static double ctof(double);
 
-struct Bad_temp { double temp; char unit; };
+struct Bad_temp // thrown on bad temperature input
+{
+     double temp;
+     char unit;
+};
 
 int main()
 {
      double t, r;
      char unit, ounit;
 
-     cin >> t;
-     cin >> unit;
+     cin >> t >> unit;
      switch (unit) {
      case 'C': case 'c':
 	  r = ctof(t);
@@ -36,11 +41,13 @@ int main()
      return 0;
 }
 
+//convert f in Fahrenheits to temperature in centigrades
 double ftoc(double f)
 {
      return (f-zeroC_in_F)/F_in_C;
 }
 
+//convert c in degrees Celsium to Fahrenheits
 double ctof(double c)
 {
      return F_in_C*c + zeroC_in_F;
